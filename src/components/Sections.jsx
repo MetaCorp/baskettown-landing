71dc9a26e05f5f67ad55b04dce587af58a54173d
DylanMerigaud <dylanmerigaud@gmail.com>
2019-05-29 16:41:41 +0200

    Add mailchimp

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -3,6 +3,9 @@ import classnames from "classnames";
 import { makeStyles } from "@material-ui/styles";
 import IconButton from "@material-ui/core/IconButton";
 import { ArrowDropDownCircleOutlined } from "@material-ui/icons";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 import Section1 from "./sections/Section1";
 
@@ -23,6 +26,57 @@ const Sections = props => {
         </IconButton>
       </div>
       <div id="section2" />
+      <MailchimpSubscribe
+        url={
+          "https://gmail.us20.list-manage.com/subscribe/post?u=8d7edf0333642e86603b9a948&id=8dc71e9e5d"
+        }
+        render={({ subscribe, status, message }) => (
+          <form onSubmitted={subscribe}>
+            <TextField
+              label="Email"
+              type="email"
+              autocapitalize="off"
+              autocorrect="off"
+              name="MERGE0"
+              id="MERGE0"
+              size="25"
+              variant="outlined"
+              placeholder="example@mail.com"
+            />
+            <TextField
+              label="First Name"
+              type="text"
+              name="MERGE1"
+              id="MERGE1"
+              size="25"
+              variant="outlined"
+              placeholder="Berthier"
+            />
+            <TextField
+              label="Last Name"
+              type="text"
+              name="MERGE2"
+              id="MERGE2"
+              size="25"
+              variant="outlined"
+              placeholder="Corinne"
+            />
+            <Button variant="contained" type="submit" />
+            {status === "sending" && (
+              <div style={{ color: "blue" }}>sending...</div>
+            )}
+            {status === "error" && (
+              <div
+                style={{ color: "red" }}
+                dangerouslySetInnerHTML={{ __html: message }}
+              />
+            )}
+            {status === "success" && (
+              <div style={{ color: "green" }}>Subscribed !</div>
+            )}
+          </form>
+        )}
+      />
     </div>
   );
 };
